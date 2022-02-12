2e0eca91d47de093aa9f9f4e06d05cf9104b3408
ttesmer <t.tesmer@gmx.de>
2022-02-12 15:48:18 +0100

    split code into Helper.hs and Network.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,29 @@
 {-# LANGUAGE BangPatterns #-}
 module Main where
 import           Control.Monad
-import           Network
 import           Numeric.LinearAlgebra
 import           System.Directory      (getCurrentDirectory)
 import           System.IO
 import           Text.Printf
 
+import           Network
+import           Helper
+
+-- | Tune hyperparameters 
+-- | of new NeuralNet below.
+-- | `batchSize` has to be a
+-- | divisor of 50000
 netConfig :: IO NeuralNet
 netConfig = initWB NN
     { weights     = []
     , biases      = []
     , eta         = 0.1
     , lambda      = 5.0
-    , totalEpochs = 60
-    , epochs      = 60
+    , totalEpochs = 30
+    , epochs      = 30
     , layers      = 2
     , layerSize   = 100
-    , batchSize   = 10  -- has to be a divisor of 50000
+    , batchSize   = 10
     , trainData   = []
     , testData    = []
     }
@@ -31,6 +37,8 @@ main = do
     net <- netConfig
 
     printf "Running cross-entropy mini-batch SGD with η of %.2f, λ of %.2f and %d neurons:\n" (eta net) (lambda net) (layerSize net)
+
+    -- train the model and test using testData or validationData
     model <- train net {trainData = trainData, testData = testData}
 
     forM_ (enum $ weights model) $ \(i, m) -> do
--- src/Network.hs
+++ src/Network.hs
@@ -1,33 +1,22 @@
 {-# LANGUAGE NamedFieldPuns  #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Network
     ( NeuralNet (..),
       train,
       initWB,
-      loadData,
-      loadTestData,
     ) where
 
-import           Codec.Compression.GZip (decompress)
 import           Control.DeepSeq
 import           Control.Lens
 import           Control.Monad
-import qualified Data.Array.IO          as A
-import qualified Data.ByteString        as BS
-import qualified Data.ByteString.Lazy   as BL
-import           Data.Int               (Int64)
 import           Data.List
 import           GHC.Base               (build)
 import           Numeric.LinearAlgebra  hiding (build, normalize)
 import           Prelude                hiding ((<>))
-import           System.Directory       (getCurrentDirectory)
-import qualified System.Random          as R
 import           Text.Printf
-import           Util
 
-type Image        = Vector Double
-type Label        = Vector Double
+import           Helper
+
 type Bias         = Vector Double
 type Weight       = Matrix Double
 type Activation   = Matrix Double
@@ -132,12 +121,6 @@ sigmoid x = 1 / (1 + exp (-x))
 sigmoid' :: Floating a => a -> a
 sigmoid' x = x * (1 - x)
 
-normalize :: (Integral a, Floating b) => a -> b
-normalize x = fromIntegral x / 255
-
-divBy :: Double -> Matrix Double -> Matrix Double
-divBy x = scale (1/x)
-
 {-- Data Processing Functions --}
 initWB :: NeuralNet -> IO NeuralNet
 initWB net@NN{batchSize = bs, layerSize = ls, layers = n} = do
@@ -154,60 +137,3 @@ initWB net@NN{batchSize = bs, layerSize = ls, layers = n} = do
     let biases = force . reverse . map flatten $ initB ++ [lastB]
 
     return net {weights=weights, biases=biases}
-
-loadData :: IO ([(Image, Label)], [(Matrix Double, Int)])
-loadData = do
-    trainImgs <- getData "train-images-idx3-ubyte.gz"
-    trainLabels <- getData "train-labels-idx1-ubyte.gz"
-    let labels = BL.toStrict trainLabels
-    let imgs = BL.toStrict trainImgs
-    let trainData = force [(getImage n imgs, vectorizeLabel $ getLabel n labels)| n <- [0..49999]]
-    let validationData = force [(asColumn $ getImage n imgs, getLabel n labels) | n <- [50000..59999]]
-    return (trainData, validationData)
-
-loadTestData :: IO [(Matrix Double, Int)]
-loadTestData = do
-    testImgs <- getData "t10k-images-idx3-ubyte.gz"
-    testLabels <- getData "t10k-labels-idx1-ubyte.gz"
-    let labels = BL.toStrict testLabels
-    let imgs = BL.toStrict testImgs
-    return [(asColumn $ getImage n imgs, getLabel n labels) | n <- [0..9999]]
-
-getData :: FilePath -> IO BL.ByteString
-getData path = do
-    currentDir <- getCurrentDirectory
-    decompress
-        <$> BL.readFile (currentDir ++ "/data/mnist_dataset/" ++ path)
-
-getImage :: Int -> BS.ByteString -> Image
-getImage n imgs = fromList [normalize $ BS.index imgs (16 + n*784 + s) | s <- [0..783]]
-
-getLabel :: Num a => Int -> BS.ByteString -> a
-getLabel n labels = fromIntegral $ BS.index labels (n+8)
-
-vectorizeLabel :: Int -> Vector Double
-vectorizeLabel l = fromList $ x ++ 1 : y
-    where (x,y) = splitAt l $ replicate 9 0
-
-{-- HELPER FUNCTIONS --}
-toMatrix :: Vector Double -> Matrix Double
-toMatrix = reshape 1
-
-sumRows :: (Num a, Element a) => Matrix a -> Vector a
-sumRows m = fromList $ sum <$> toLists m
-
--- | Randomly shuffle a list
---   /O(N)/
-shuffle :: [a] -> IO [a]
-shuffle xs = do
-        ar <- newArray n xs
-        forM [1..n] $ \i -> do
-            j <- R.randomRIO (i,n)
-            vi <- A.readArray ar i
-            vj <- A.readArray ar j
-            A.writeArray ar j vi
-            return vj
-  where
-    n = length xs
-    newArray :: Int -> [a] -> IO (A.IOArray Int a)
-    newArray n xs =  A.newListArray (1,n) xs
